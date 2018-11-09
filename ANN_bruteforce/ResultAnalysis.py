f7fc35635c51ead39f51a69dbef9087fe23920ae
Christoph Schaefer <schaefer.christoph@outlook.com>
2018-11-09 17:03:37 +0100

    finished test for benchmark ANN (RELU,3layers,10bootstrap) epurate maybe? also added ROC curves, still need threshold dep confu matrix

--- ANN_bruteforce/ResultAnalysis.py
+++ ANN_bruteforce/ResultAnalysis.py
@@ -1,3 +1,5 @@
+"Credit Christoph Schaefer christophernstrerne.schaefer@epfl.ch, github: https://github.com/cerschae "
+
 import util
 import pandas as pd
 # TensorFlow and tf.keras
@@ -14,6 +16,9 @@ from keras.callbacks import TensorBoard
 import h5py
 from sklearn.metrics import confusion_matrix
 from sklearn.externals import joblib
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
+from itertools import cycle
 
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
@@ -30,7 +35,7 @@ def plot_confusion_matrix(cm, classes,
     else:
         print('Confusion matrix, without normalization')
 
-    print(cm)
+    #print(cm)
 
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
@@ -49,6 +54,9 @@ def plot_confusion_matrix(cm, classes,
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.tight_layout()
+
+
+
 
 def predict_lightcurves(model,x_test,y_test):
     """
@@ -75,7 +83,7 @@ def predict_lightcurves(model,x_test,y_test):
     plt.figure()
     plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,title='Normalized confusion matrix')
 
-    plt.show()
+    #plt.show()
     return y_pred
 
 if __name__ == "__main__":
@@ -104,6 +112,67 @@ if __name__ == "__main__":
     print("Score: ", score)
 
 
-    y_test = predict_lightcurves(model,x_test,y_test)
+    y_score = predict_lightcurves(model,x_test,y_test)
+
+    class_names = np.array([6, 15, 16, 42, 52, 53, 62, 64, 65, 67, 88, 90, 92, 95, 99])
+    n_classes = len(class_names)
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # Compute macro-average ROC curve and ROC area
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    plt.figure()
+    plt.plot(fpr["micro"], tpr["micro"],
+             label='micro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["micro"]),
+             color='deeppink', linestyle=':', linewidth=4)
+
+    plt.plot(fpr["macro"], tpr["macro"],
+             label='macro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["macro"]),
+             color='navy', linestyle=':', linewidth=4)
+
+    colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+    lw = 2
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(fpr[i], tpr[i], lw=lw,
+                 label='ROC curve of class {0} (area = {1:0.2f})'
+                       ''.format(class_names[i], roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Multi-class Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
     print(y_test)
 
