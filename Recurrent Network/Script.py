1e56c13052bbedf72d1d120c536c32a2fdda5425
Christoph Schaefer <schaefer.christoph@outlook.com>
2018-11-29 11:24:51 +0100

    further training

--- Recurrent Network/Script.py
+++ Recurrent Network/Script.py
@@ -18,6 +18,7 @@ from keras.layers import Dropout
 "Taken from https://machinelearningmastery.com/sequence-classification-lstm-recurrent-neural-networks-python-keras/"
 
 # Recurrent Network
+model_name = "run_RecNN_Feat50_Dropout05_StandY_boot5_epu5_mjdsY_rmsprop_lr0.001_decay0.1"
 
 """  Load Data """
 init_data = util.readpickle('../training_samples.pkl')
@@ -26,6 +27,7 @@ test = init_data.loc[:1000].copy(deep=True)
 
 #Dataset augmentation
 data = dataset_augmentation(data_start, bootstrapping=5, epurate=5)
+
 
 """ Data Preparation """
 ## What type of data input needed ?
@@ -64,10 +66,12 @@ rmsprop = keras.optimizers.RMSprop(lr=0.001, decay=0.01)
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 print(model.summary())
 
-tbCallBack = keras.callbacks.TensorBoard(log_dir='./logs/run_RecNN_Feat50_Dropout05_StandY_boot5_epu5_mjdsY_rmsprop_lr0.001_decay0.1', histogram_freq=0,
+tbCallBack = keras.callbacks.TensorBoard(log_dir='./logs/' + model_name, histogram_freq=0,
                                          write_graph=True, write_images=True)
 
 history = model.fit(x_train, y_train, epochs=50, validation_split=0.2, batch_size=32, verbose=1,callbacks = [tbCallBack])
+
+model.save(model_name)
 
 # Final evaluation of the model
 scores = model.evaluate(x_test, y_test, verbose=0)
@@ -79,30 +83,10 @@ print("Accuracy: ", scores)
 
 
 """
-# fix random seed for reproducibility
-numpy.random.seed(7)
-# load the dataset but only keep the top n words, zero the rest
-top_words = 5000
-(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
-print(X_train)
-print(y_train)
-print(X_train.shape)
-# truncate and pad input sequences
-max_review_length = 500
-X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
-X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
-print(X_train)
-print(X_train.shape)
-# create the model
-embedding_vector_length = 32
-model = Sequential()
-model.add(Embedding(top_words, embedding_vector_length, input_length=max_review_length))
-model.add(LSTM(100))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
-#model.fit(X_train, y_train, epochs=3, batch_size=64)
-# Final evaluation of the model
-#scores = model.evaluate(X_test, y_test, verbose=0)
-#print("Accuracy: %.2f%%" % (scores[1]*100))
+Next:
+Test GRU
+Test RecNN
+Test Standardisation
+Test +ANN
+Test + z
 """
