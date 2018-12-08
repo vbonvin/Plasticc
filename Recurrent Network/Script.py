39ee55090ec55597727e4718198c8e3f1f1fd83f
Christoph Schaefer <schaefer.christoph@outlook.com>
2018-12-08 09:57:54 +0100

    one more network

--- Recurrent Network/Script.py
+++ Recurrent Network/Script.py
@@ -34,7 +34,7 @@ def compute_weights(N_of_Classes, penalty_factor=20):
 "Taken from https://machinelearningmastery.com/sequence-classification-lstm-recurrent-neural-networks-python-keras/"
 
 # Recurrent Network
-model_name = "LSTM_P20_Feat50_B0E0_Number5"
+model_name = "LSTM_P0_Feat50_B0E0"
 
 """  Load Data """
 init_data = util.readpickle('../training_samples_astest.pkl')
@@ -88,7 +88,7 @@ def w_categorical_crossentropy(y_true, y_pred, weights):
 
 ## Creating Weights: the critical and arbitrary factor is the penalty_factor
 N_of_Classes = np.array([151., 495., 924., 1193., 183., 30., 484., 102., 981.,  208., 370.,  2313.,  239., 175.])
-penalty_factor = 20
+penalty_factor = 0
 
 w_array = compute_weights(N_of_Classes, penalty_factor=penalty_factor)
 
@@ -110,13 +110,13 @@ print(model.summary())
 tbCallBack = keras.callbacks.TensorBoard(log_dir='./logs/' + model_name, histogram_freq=0,
                                          write_graph=True, write_images=True)
 #Continue Training
-interrupted_model_name = "LSTM_P20_Feat50_B0E0_Number3"
-model.load_weights(interrupted_model_name + '_model_weights.h5')
+#interrupted_model_name = "LSTM_P20_Feat50_B0E0_Number3"
+#model.load_weights(interrupted_model_name + '_model_weights.h5')
 
 modelcheckpointCallBack = keras.callbacks.ModelCheckpoint('weights{epoch:08d}.h5',
                                      save_weights_only=True, period=10)
 
-history = model.fit(x_train, y_train, epochs=100, validation_data=(x_test, y_test), batch_size=32, verbose=1,callbacks = [tbCallBack,modelcheckpointCallBack])
+history = model.fit(x_train, y_train, epochs=200, validation_data=(x_test, y_test), batch_size=32, verbose=1,callbacks = [tbCallBack,modelcheckpointCallBack])
 
 # serialize model to JSON
 model_json = model.to_json()
